010e34f31d66ebfff42882e3860ad9412fc7ab87
Berk Gülşen <83736291+berkgulsen@users.noreply.github.com>
2022-10-01 18:28:23 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Berk Gülşen on 1.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
